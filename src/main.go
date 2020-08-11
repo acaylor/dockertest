dc5d6697ea1cabc7454b5d2c4073d6e68a73c609
emrakool <ajcaylor@gmail.com>
2020-08-11 10:40:15 -0400

    comments and change message

--- src/main.go
+++ src/main.go
@@ -8,26 +8,26 @@ import (
 )
  
 const homepageEndPoint = "/"
- 
-// StartWebServer the webserver
+
 func StartWebServer() {
     http.HandleFunc(homepageEndPoint, handleHomepage)
+    // port is set through env variable
     port := os.Getenv("PORT")
     if len(port) == 0 {
         panic("Environment variable PORT is not set")
     }
- 
+    // log attempt to start web server
     log.Printf("Starting web server to listen on endpoints [%s] and port %s",
         homepageEndPoint, port)
     if err := http.ListenAndServe(":"+port, nil); err != nil {
         panic(err)
     }
 }
- 
+// return message on bound webserver port 
 func handleHomepage(w http.ResponseWriter, r *http.Request) {
     urlPath := r.URL.Path
     log.Printf("Web request received on url path %s", urlPath)
-    msg := "Hello world"
+    msg := "Hello there"
     _, err := w.Write([]byte(msg))
     if err != nil {
         fmt.Printf("Failed to write response, err: %s", err)
